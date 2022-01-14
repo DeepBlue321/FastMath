503efa57f62f4e7f846ea9f13bbc24dc8eaa859f
DeepBlue321 <frantisek1nemet@gmail.com>
2022-01-15 00:28:08 +0100

    adding nested menu

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,24 +1,83 @@
 
 
-import * as React from 'react';
 
 import ClearIcon from '@mui/icons-material/Clear';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
-import { Button, Stack } from "@mui/material";
+import { Button, Collapse, List, ListItemButton, ListItemIcon, ListItemText, Stack } from "@mui/material";
+import React,{ useState } from 'react';
+import { CollectionsOutlined, ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
 
 function Menu() {
-  return (
-    <div>
-      
-       
+  
+  const menuObject =[
+    {
+      name: "Sčítání",
+      elements:["2by2","_ _ _ + _ _ _"],
+      icon:  <AddIcon />,
+      open :false
+    } ,
+    {
+      name: "Násobení",
+      elements:["_ + _","_ _ _ + _ _ _"],
+      icon: <ClearIcon />,
+      open:false
+    }       
+    
+  ];
+  
+  const [list, setList] = useState(menuObject);
 
-      <Stack direction="column" spacing={2}>
-      <Button startIcon={<ClearIcon />}  href="/multiply">Násobení</Button>
+  const handleClick = (name) => {
+     const newList = list.map((item) => {
+      if (item.name === name) {
+        const updatedItem = {
+          ...item,
+          open: !item.open,
+        };
+
+        return updatedItem;
+      }
+
+      return item;
+    }); 
+    setList(newList);
+  };
+  return (
+  
+    <Stack direction="column" spacing={2}>
+{/*       <Button startIcon={<ClearIcon />}  href="/multiply">Násobení</Button>
       <Button  startIcon={<RemoveIcon />}href="/sub">Odečítánní</Button>
-      <Button startIcon={<AddIcon />} href="/add">Sčítání</Button>
+      <Button startIcon={<AddIcon />} href="/add">Sčítání</Button> */}
+       { list.map((t,k) => (
+         <div key={t.name} >
+           {console.log(t)}
+              <ListItemButton  onClick={()  => handleClick(t.name)} >  
+      <ListItemIcon>
+         {t.icon}
+        </ListItemIcon>
+        <ListItemText primary={t.name} />
+        {t.open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={t.open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+     {   t.elements.map((elem) =>(
+            <ListItemButton key={elem} sx={{ pl: 4 }}>
+           
+            <ListItemText primary={elem} />
+          </ListItemButton>
+     ))}
+        
+
+        </List>
+      </Collapse>
+         </div>
+         
+       )) }    
+
+   
     </Stack>
-    </div>
+   
   );
 }
 
---
+++ src/component/Score.js
@@ -0,0 +1,29 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    container: {
+        position : "absolute",
+        top: "20px",
+        right:"20px",
+        textAlign:"right"
+    }
+})
+function Score({score}) {
+
+    const styles = useStyles();
+  
+    return (
+        <Container className={styles.container}>
+            <Typography>
+              Skore:  {score}
+            </Typography >
+            <Typography>
+               Nejvyší skore: {0}
+            </Typography>
+        </Container>
+    )
+}
+
+export default Score
